1ef6e19bf46c469413e2cd2578d239e4e09ed5f3
LereRentesca <esteban.renteria@uabc.edu.mx>
2024-10-10 20:04:57 -0700

    Class #3

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,6 @@ import posts.views as view
 
 urlpatterns = [
     path("", view.HomePageView.as_view(), name="home"),
+    path("posts/", view.PostListView.as_view(), name="posts"),
     path("about/", view.AboutPageView.as_view(), name="about"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,19 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_list"] = (
+            Post.objects.order_by("date").reverse()
+        )
+        return context
